1e543223dfe9f53c127b304a7e4b01ceea4fc075
Dan Haman <uef_mailbox@yahoo.co.uk>
2016-05-07 01:21:25 +0300

    + basic l293d based engine control implemented + serial logger added

--- on-board/on-board.ino
+++ on-board/on-board.ino
@@ -1,9 +1,36 @@
+#define LOG_BAUDRATE 9600
+
+#define PIN_ENGINE_SWITCH 8
+#define PIN_ENGINE_POSITIVE 9
+#define PIN_ENGINE_NEGATIVE 10
+#define PIN_GEARBOX_SWITCH 11
+
+#define ENGINE_FORWARD HIGH
+#define ENGINE_REVERSE LOW
+#define ENGINE_RUN 1
+#define ENGINE_STOP 0
+
 void setup() {
-  // put your setup code here, to run once:
+  configureLogger();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
+
+void controlEngine(uint8_t status, uint8_t direction) {
+  digitalWrite(PIN_ENGINE_SWITCH, (status ? HIGH : LOW));
+  digitalWrite(PIN_ENGINE_POSITIVE, direction);
+  digitalWrite(PIN_ENGINE_NEGATIVE, !direction); 
+}
+
+void configureLogger() {
+  Serial.begin(LOG_BAUDRATE);
+}
+
+void logMessage(uint8_t severity, char *message) {
+  Serial.print(severity);
+  Serial.print(" : ");
+  Serial.println(message);
+}
+
