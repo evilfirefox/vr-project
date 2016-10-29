a9369361b722f46ef5d6f37f6a66c0f74682b109
Dan Haman <uef_mailbox@yahoo.co.uk>
2016-10-29 21:32:57 +0300

    + peripherals container integrated + some test commands in base

--- platformio-vr/src/vr.ino
+++ platformio-vr/src/vr.ino
@@ -1,10 +1,12 @@
 #include <stdint.h>
 #include <SPI.h>
 #include <RH_ASK.h>
+#include "UnitPeripheralsContainer.h"
+#include "CommandHandlerFactory.h"
 #include "RadioCommandFactory.h"
 
 #define SERIAL_BAUDRATE 9600
-#define RADIO_BAUDRATE 2000
+#define RADIO_BAUDRATE 9600
 #define PIN_RX 11
 #define PIN_TX 12
 #define BUFFER_SIZE 8
@@ -13,7 +15,10 @@ RH_ASK radio = RH_ASK(RADIO_BAUDRATE, PIN_RX, PIN_TX);
 uint8_t buffer[BUFFER_SIZE];
 uint8_t size = sizeof(buffer);
 
+Motor motor = Motor();
+UnitPeripheralsContainer container = UnitPeripheralsContainer();
 RadioCommandFactory commandFactory = RadioCommandFactory();
+CommandHandlerFactory handlerFactory = CommandHandlerFactory(&container);
 
 void setup() {
   Serial.begin(SERIAL_BAUDRATE);
@@ -21,12 +26,18 @@ void setup() {
   pinMode(PIN_TX, OUTPUT);
   radio.init();
   radio.setModeRx();
+
+  container.setMotor(&motor);
 }
 
 void loop() {
   if (radio.available()) {
     if (radio.recv(buffer, &size)) {
       Command* command = commandFactory.createCommand(buffer, size);
+      uint8_t* d = command->getData();
+      Serial.println(*d, BIN);
+      CommandHandler* handler = handlerFactory.createCommandHandler(command);
+      handler->process(command);
     }
   }
 }
